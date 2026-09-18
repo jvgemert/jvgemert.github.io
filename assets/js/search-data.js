caeabb132c312aa7ead02887029d9b844582f22c
jvgemert <nietpiet@yahoo.com>
2026-09-18 11:47:04 +0000

    Deploying to gh-pages from @ jvgemert/jvgemert.github.io@5015b42f94a3de3aa704768d89c6c39a49089e8f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,6 +37,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/meeting.html";
           },
+        },{id: "nav-teaching",
+          title: "teaching",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/_pages/teach.html";
+          },
         },{id: "news-accepted-paper-icip-2015-per-patch-metric-learning-for-robust-image-matching-pdf",
           title: 'Accepted paper. ICIP 2015, “Per-patch metric learning for robust image matching”  (pdf)',
           description: "",
